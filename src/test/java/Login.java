cfa7bf0798a9e9bb6d1071808b676b937c575ff7
sK4ri <skari.simon@gmail.com>
2019-08-21 11:50:26 +0200

    checkValidLogin

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,12 +1,42 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import java.util.concurrent.TimeUnit;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Login {
 
-    @AfterEach
+    private static WebDriver driver;
+
+    @BeforeEach
+    private void openDriver() {
+        driver = new ChromeDriver();
+        driver.get("https://jira.codecool.codecanvas.hu/");
+    }
+
+//    @AfterEach
+//    private void closeDriver() {
+//        driver.close();
+//    }
 
     @Test
     public void checkValidLogin() {
+
+        Wait wait = new FluentWait(driver).ignoring(NoSuchElementException.class).withTimeout(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("login-form-username")).sendKeys("user18");
+        driver.findElement((By.id("login-form-password"))).sendKeys("CoolCanvas19.");
+        driver.findElement(By.id("login")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("header-details-user-fullname")));
+        WebElement userID = driver.findElement(By.id("header-details-user-fullname"));
+        assertEquals("user18", userID.getAttribute("data-username"));
 
     }
 }
